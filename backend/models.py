d813dbb727cc9cac9d20e354f5a8392ac3d2f7cf
maurineA <maurinawambuia@gmail.com>
2024-02-06 09:47:13 +0300

    created the modelus products, shopproducts and shop

--- backend/models.py
+++ backend/models.py
@@ -2,6 +2,37 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class  shop(db.Model):
-    id=  db.Column(db.Integer, primary_key=True)
+class Shop(db.Model):
+    __tablename__ = "shops"
+    id =  db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String)
+    shopname = db.Column(db.String)
+    address = db.Column(db.String)
+    contact = db.Column(db.Integer)
+    
+    Shopproduct = db.relationship("Shopproduct", backref="Shopproduct")
+
+class Shopproduct(db.Model):
+    __tablename__ ="shopproducts"
+    id =  db.Column(db.Integer, primary_key=True)
+    shop_id = db.Column(db.Integer, db.foreignKey("shops.id"))
+    product_id = db.Column(db.Integer, db.foreignKey("products.id"))
+    price = db.Column(db.Integer)
+
+
+
+class Product(db.Model):
+    __tablename__ ="products"
+    id =  db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    description = db.Column(db.Text)
+    quantity = db.Column(db.String)
+    image = db.Column(db.String)
+    
+
+    Shopproduct = db.relationship("Shopproduct", backref="product")
+
+
+
+
     
