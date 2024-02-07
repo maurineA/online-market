73cbefc9c684a1a64c81e4b90a598f98a6db94e7
maurineA <maurinawambuia@gmail.com>
2024-02-07 11:16:37 +0300

    added the route for post method

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 
 import os
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, make_response, request
 from flask_migrate import Migrate
 from flask_cors import CORS
 from models import db, Shop, Product
@@ -99,7 +99,41 @@ def get_product(id):
         response = {"error": "id not found"}
         return jsonify(response), 404
 
+@app.route("/addshop", methods=["POST"])
+def post_shop():
+    data = request.json
+    username = data.get("username")
+    shopname = data.get("shopname")
+    address = data. get("address")
+    contact = data.get("contact")
+
+    if not all([username, shopname, address, contact]):
+        return jsonify({"error": "missing parameter"}),400
+    new_shop = Shop(
+        username = username,
+        shopname = shopname,
+        address = address,
+        contact = contact
+
+    )
+
+    db.session.add(new_shop)
+    db.session.commit()
+
+    shop_data = {
+        "id": new_shop.id,
+        "username": new_shop.username,
+        "shopname": new_shop.shopname,
+        "address": new_shop.address,
+        "contact":new_shop.contact,
+    }
+
+    response = make_response(jsonify(shop_data),200) 
+    return response
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True,port=5555)
-    
