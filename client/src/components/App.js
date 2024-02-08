27eea04e81c30d3412e04d8666cf911349f47afd
33-66 <simon.nganga@student.moringaschool.com>
2024-02-08 11:37:56 +0300

    Login page and footer added

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,17 +5,20 @@ import { Route, Routes } from 'react-router-dom';
 import Shops from './Shops';
 import ShopProducts from './ShopProducts';
 import NewShop from './NewShop';
+import Footer from './Footer';
+import Login from './Login';
 function App() {
   return (
     <div>
         <NavBar />
         <Routes>
-        <Route path='/' element={<Home/>}></Route>
+        <Route path='/' element={<Login/>}></Route>
         <Route path='/home' element={<Home/>}></Route>
         <Route path='/shops' element={<Shops/>}></Route>
         <Route path='/shops/:productsId' element={<ShopProducts/>}></Route>
         <Route path='/newShop' element={<NewShop/>}></Route>
         </Routes>
+        <Footer/>
     </div>
   )
 }
