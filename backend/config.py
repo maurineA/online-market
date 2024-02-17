f3b1434db1ea3507b745badc426123ea6ad1495b
maurineA <maurinawambuia@gmail.com>
2024-02-17 13:23:29 +0300

    config

--- backend/config.py
+++ backend/config.py
@@ -11,6 +11,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # app.json.compact = False
 
+
 db = SQLAlchemy()
 migrate = Migrate(app, db)
 db.init_app(app)
