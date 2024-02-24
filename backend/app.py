212c937bf693c7b1a913985ee467c5b833c0beab
33-66 <simon.nganga@student.moringaschool.com>
2024-02-24 16:45:19 +0300

    backend  deployment

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
 from flask import jsonify, make_response, request, session, url_for
-from models import User, Shop, Product, Shopproduct
+from  models import User, Shop, Product, Shopproduct
 
 from config import app,db
 
